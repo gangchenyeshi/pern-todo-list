b35357f2afd28bda592f1927600b69950e24f3a6
Yeshi Tsering Gangchen <yestsering@gmail.com>
2021-08-31 15:59:56 +0200

    devConfig in database

--- db.js
+++ db.js
@@ -10,8 +10,13 @@ require('dotenv').config();
 // }
 //OR
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
-
-const proConfig = connectionString = process.env.DATABASE_URL
+const proConfig = {
+    connectionString: process.env.DATABASE_URL, //heroku addon
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+// const proConfig = connectionString = process.env.DATABASE_URL
 //it is heroku addons
 
 const pool = new Pool ({
