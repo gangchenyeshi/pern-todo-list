eb551191cb8d786b626038eb88df544e9eb0aa94
Yeshi Tsering Gangchen <yestsering@gmail.com>
2021-08-31 16:58:02 +0200

    chages proconfig

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 //OR
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
-const proConfig = process.env.HEROKU_POSTGRESQL_AQUA_URL
+const proConfig = process.env.DATABASE_URL
 //it is heroku addons
 
 const pool = new Pool ({
